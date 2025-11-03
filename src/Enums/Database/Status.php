9a4c5e362063898c020585e799bd80429110128d
Sebastian Sulinski <seb@sulinski.me>
2025-11-03 12:48:13 +0000

    Add additional statuses to `Status` enum in `Database` class

--- src/Enums/Database/Status.php
+++ src/Enums/Database/Status.php
@@ -4,7 +4,23 @@ namespace SebastianSulinski\LaravelForgeSdk\Enums\Database;
 
 enum Status: string
 {
+    case Deleting = 'deleting';
+    case Disabled = 'disabled';
+    case Disabling = 'disabling';
+    case Enabling = 'enabling';
+    case Failed = 'failed';
+    case FailedRunner = 'failed-runner';
+    case FailedUnknown = 'failed-unknown';
     case Installed = 'installed';
     case Installing = 'installing';
     case Removing = 'removing';
+    case Restarting = 'restarting';
+    case Restoring = 'restoring';
+    case Running = 'running';
+    case Starting = 'starting';
+    case Stopped = 'stopped';
+    case Stopping = 'stopping';
+    case Success = 'success';
+    case Syncing = 'syncing';
+    case Updating = 'updating';
 }
